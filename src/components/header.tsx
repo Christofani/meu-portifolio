fe27d77b57af3a828dbdbf75e6300d0b0379f07c
Rodrigo Christofani <christofanir@gmail.com>
2024-11-11 11:54:33 -0300

    Estrutura conclúida.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,36 +1,43 @@
 import { useState } from 'react';
-import Flag from 'react-world-flags';
+import { FlagIcon } from 'react-flag-kit';
 
 function Header() {
-  const [language, setLanguage] = useState('pt-BR'); // Estado para o idioma
+  const [language, setLanguage] = useState('pt-BR');
 
-  const handleLanguageChange = (e) => {
+  const handleLanguageChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setLanguage(e.target.value);
     // Aqui você pode adicionar a lógica para mudar o idioma do conteúdo do seu portfólio
   };
 
   return (
     <header className="bg-slate-700 flex justify-between items-center p-5">
-      {/* Select de idioma com bandeiras usando react-world-flags */}
-      <select
-        value={language}
-        onChange={handleLanguageChange}
-        className="bg-gray-800 text-white p-2 rounded-full"
-      >
-        <option value="pt-BR" className="flex items-center">
-          <Flag code="BR" className="mr-2" /> PT-BR
-        </option>
-        <option value="en-US" className="flex items-center">
-          <Flag code="US" className="mr-2" /> EN-US
-        </option>
-      </select>
+      {/* Select de idioma com bandeiras */}
+      <div className="flex items-center">
+        <FlagIcon code={language === 'pt-BR' ? 'BR' : 'US'} className="mr-2 w-6 h-6" />
+        <select
+          value={language}
+          onChange={handleLanguageChange}
+          className="bg-gray-800 text-white p-2 rounded-full"
+        >
+          <option value="pt-BR">PT-BR</option>
+          <option value="en-US">EN-US</option>
+        </select>
+      </div>
 
       <nav>
-        <ul className="flex space-x-10 list-none p-0">
-          <li><a href="#sobre" className="text-white">Home</a></li>
-          <li><a href="#projetos" className="text-white">Sobre</a></li>
-          <li><a href="#habilidades" className="text-white">Projetos</a></li>
-          <li><a href="#contato" className="text-white">Contato</a></li>
+        <ul className="flex space-x-6 md:space-x-10 list-none p-0 text-sm md:text-base">
+          <li>
+            <a href="#home" className="text-white hover:text-gray-300 transition-colors duration-200">Home</a>
+          </li>
+          <li>
+            <a href="#sobre_mim" className="text-white hover:text-gray-300 transition-colors duration-200">Sobre</a>
+          </li>
+          <li>
+            <a href="#projetos" className="text-white hover:text-gray-300 transition-colors duration-200">Projetos</a>
+          </li>
+          <li>
+            <a href="#contato" className="text-white hover:text-gray-300 transition-colors duration-200">Contato</a>
+          </li>
         </ul>
       </nav>
     </header>
